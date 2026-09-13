9a1b165250160ba0241a853dfc25cb3d963ad0a3
Nurul Amin Khondoker <nurulaminkh33@gmail.com>
2026-09-14 01:55:51 +0600

    added profiling report form along with the profiling engine

--- DICPS/Forms/ChiefInvestigator/ProfilingReportForm.cs
+++ DICPS/Forms/ChiefInvestigator/ProfilingReportForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DICPS.Common;
+using DICPS.Models.ChiefInvestigator;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +9,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DICPS.Common;
+using DICPS.Models.ChiefInvestigator;
 
 namespace DICPS.Forms.ChiefInvestigator
 {
     public partial class ProfilingReportForm : Form
     {
-        public ProfilingReportForm()
+        private StaffUser loggedInUser;
+        private ProfilingEngine engine = new ProfilingEngine();
+        public ProfilingReportForm(StaffUser user)
         {
             InitializeComponent();
+            loggedInUser = user;
+        }
+
+        private void ProfilingReportForm_Load(object sender, EventArgs e)
+        {
+            var suspects = engine.GetAllSuspects();
+            cmbSuspect.DataSource = suspects;
+            cmbSuspect.DisplayMember = "Name";
+            cmbSuspect.ValueMember = "SuspectID";
+
+            var cases = engine.GetAllCases();
+            cmbCase.DataSource = cases;
+            cmbCase.DisplayMember = "CaseNumber";
+            cmbCase.ValueMember = "CaseID";
+        }
+
+        private void btnGenerateScore_Click(object sender, EventArgs e)
+        {
+            int suspectId = Convert.ToInt32(cmbSuspect.SelectedValue);
+            int caseId = Convert.ToInt32(cmbCase.SelectedValue);
+
+            float score = engine.GenerateProfilingScore(suspectId, caseId);
+            lblScoreResult.Text = "Profiling Score: " + score;
+
+            ProfilingReport report = new ProfilingReport
+            {
+                CaseId = caseId,
+                SuspectId = suspectId,
+                ReviewedByUserId = loggedInUser.UserId,
+                ProfilingScore = score
+            };
+            report.SaveProfilingReport();
+
+            MessageBox.Show("Profiling report generated and saved.");
+        }
+
+        private void btnViewReports_Click(object sender, EventArgs e)
+        {
+            int caseId = Convert.ToInt32(cmbCase.SelectedValue);
+            ProfilingReport report = new ProfilingReport();
+            dgvReports.DataSource = report.GetProfilingReportByCase(caseId);
         }
     }
 }
