33395da08b765cc06cc81a9f96ba019477dbff84
Nurul Amin Khondoker <nurulaminkh33@gmail.com>
2026-09-15 00:15:10 +0600

    DetectiveDashboardForm.cs functions done

--- DICPS/Forms/Detective/DetectiveDashboardForm.cs
+++ DICPS/Forms/Detective/DetectiveDashboardForm.cs
@@ -1,45 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using DICPS.Common;
+using DICPS.Models.Detective;
+using DICPS.Forms.Shared;
 
 namespace DICPS.Forms.Detective
 {
     public partial class DetectiveDashboardForm : Form
     {
-        public DetectiveDashboardForm()
+        private StaffUser loggedInUser;
+
+        public DetectiveDashboardForm(StaffUser user)
         {
             InitializeComponent();
+            loggedInUser = user;
         }
 
         private void DetectiveDashboardForm_Load(object sender, EventArgs e)
         {
+            lblLoggedInUserId.Text = "User ID: " + loggedInUser.UserId;
+            lblLoggedInName.Text = "Name: " + loggedInUser.Name;
 
+            LoadStatistics();
+        }
+
+        private void LoadStatistics()
+        {
+            lblCaseCount.Text = DICPS.Models.Detective.Detective.GetCaseCount(loggedInUser.UserId).ToString();
+            lblSuspectCount.Text = DICPS.Models.Detective.Detective.GetSuspectCount().ToString();
+            lblWitnessCount.Text = DICPS.Models.Detective.Detective.GetWitnessCount().ToString();
         }
 
         private void btnCaseLoad_Click(object sender, EventArgs e)
         {
-
+            CaseLoadForm form = new CaseLoadForm(loggedInUser);
+            form.Show();
         }
 
         private void btnPersonSearch_Click(object sender, EventArgs e)
         {
-
+            PersonSearchForm form = new PersonSearchForm(loggedInUser);
+            form.Show();
         }
 
         private void btnRegisterPerson_Click(object sender, EventArgs e)
         {
-
+            RegisterPersonForm form = new RegisterPersonForm(loggedInUser);
+            form.Show();
         }
 
         private void btnLogout_Click(object sender, EventArgs e)
         {
-
+            LoginForm login = new LoginForm();
+            login.Show();
+            this.Close();
         }
     }
 }
