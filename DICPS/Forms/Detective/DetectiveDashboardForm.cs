985b61b1aab96e57865d422b845171e91f6691d7
Nurul Amin Khondoker <nurulaminkh33@gmail.com>
2026-09-14 23:45:59 +0600

    Designed Detective Dashboard

--- DICPS/Forms/Detective/DetectiveDashboardForm.cs
+++ DICPS/Forms/Detective/DetectiveDashboardForm.cs
@@ -16,5 +16,30 @@ namespace DICPS.Forms.Detective
         {
             InitializeComponent();
         }
+
+        private void DetectiveDashboardForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCaseLoad_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPersonSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegisterPerson_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
